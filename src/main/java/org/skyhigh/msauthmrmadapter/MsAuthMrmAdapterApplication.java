39d08e50da047ae1085f2edbf622b1e2e540a029
Daniil Popov <popovdanin@gmail.com>
2024-08-22 13:18:52 +0300

    Initial commit

---
+++ src/main/java/org/skyhigh/msauthmrmadapter/MsAuthMrmAdapterApplication.java
@@ -0,0 +1,13 @@
+package org.skyhigh.msauthmrmadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuthMrmAdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsAuthMrmAdapterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/skyhigh/msauthmrmadapter/MsAuthMrmAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skyhigh.msauthmrmadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthMrmAdapterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
